f776439dcc20117ab8575b2fbb488efb64f6b05e
elisfreire1 <elisfreire1@gmail.com>
2025-01-22 15:15:31 +0100

    Lógica com JS: selecionando elements

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+//Selecionando os elementos do html:
+const previousOperandTextElement = document.querySelector('#previous-operand');
+const currentOperandTextElement = document.querySelector('#current-operand');
+const numberButtons = document.querySelectorAll('#buttons-container button');
