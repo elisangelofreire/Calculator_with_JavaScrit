b32f756bd665e4fdb40377cf74ab2ff3c7674445
elisfreire1 <elisfreire1@gmail.com>
2025-01-22 16:03:28 +0100

    Lógica com JS: verificando se o user clicou em números ou nos sinais

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,16 @@ class Calculator {
 
 //Criando os eventos dos buttons para pegar os seus valores, no caso, numeros e sinais:
 numberButtons.forEach((btn) => {
+    //Com o evento click, pegamos o valor do button clicado para futura operação:
     btn.addEventListener('click', (e) =>{
-        const number = e.target.innerText;
-        console.log(number);
-    })
-})
+        const btnValue = e.target.innerText;
+        
+        //Verificando se o valor é um número ou um sinal:
+        if (+btnValue >= 0 || btnValue === '.') { //o + antes da variável btnValue converte a string em número
+            console.log(btnValue);
+        } else {
+            console.log('Sinal: ', btnValue);            
+        }
+
+    });
+});
