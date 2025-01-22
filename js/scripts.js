cb0bfabbf6894f0d6d8241bae3aa180d3886c0ae
elisfreire1 <elisfreire1@gmail.com>
2025-01-22 16:05:38 +0100

    Lógica com JS: atualizando os comentários

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-//Selecionando os elementos do html:
+//Step01: Selecionando os elementos do html:
 const previousOperandTextElement = document.querySelector('#previous-operand');
 const currentOperandTextElement = document.querySelector('#current-operand');
 const numberButtons = document.querySelectorAll('#buttons-container button');
 
-//Criando a classe Calculator:
+//Step02: Criando a classe Calculator:
 class Calculator {
 
 }
 
-//Criando os eventos dos buttons para pegar os seus valores, no caso, numeros e sinais:
+//Step03: Criando os eventos dos buttons para pegar os seus valores, no caso, numeros e sinais:
 numberButtons.forEach((btn) => {
-    //Com o evento click, pegamos o valor do button clicado para futura operação:
+    //Step03.1: Com o evento click, pegamos o valor do button clicado para futura operação:
     btn.addEventListener('click', (e) =>{
         const btnValue = e.target.innerText;
         
-        //Verificando se o valor é um número ou um sinal:
+        //Step03.2: Verificando se o valor é um número ou um sinal:
         if (+btnValue >= 0 || btnValue === '.') { //o + antes da variável btnValue converte a string em número
             console.log(btnValue);
         } else {
