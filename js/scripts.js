28ee011202b75397d62dbf1628a3edb73c234cdc
elisfreire1 <elisfreire1@gmail.com>
2025-01-22 15:17:20 +0100

    Lógica com JS: criando a class Calculator

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@
 const previousOperandTextElement = document.querySelector('#previous-operand');
 const currentOperandTextElement = document.querySelector('#current-operand');
 const numberButtons = document.querySelectorAll('#buttons-container button');
+
+//Criando a classe Calculator:
+class Calculator {
+
+}
+
+//Criando os eventos dos buttons:
